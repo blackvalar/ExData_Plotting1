56e6b2ac8a98aa80cd21a8cbd90ebf836696283f
blackvalar <blackvalar@gmail.com>
2014-06-09 00:19:10 +1100

    no message

--- plot3.R
+++ plot3.R
@@ -5,3 +5,12 @@ dat$Date <- as.Date(dat$Date, format="%d/%m/%Y")
 #data filter
 dat <- dat[dat$Date >= as.Date("2007-02-01")& dat$Date<=as.Date("2007-02-02"),]
 #create graf
+png(filename="plot3.png", width=480, height=480, units="px")
+with(dat, {
+  plot(Sub_metering_1,type="l", xaxt="n", xlab="", ylab="Energy sub metering")
+  lines(x=Sub_metering_2, col="red")
+  lines(x=Sub_metering_3, col="blue")
+})
+axis(1, at=c(1, as.integer(nrow(dat)/2), nrow(dat)), labels=c("Thu", "Fri", "Sat"))
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
